5e6368919207dab5f75d8c4a0f972f84ca0e838c
Michal Haracewiat <m.haracewiat@gmail.com>
2017-12-30 17:16:49 +0100

    Update index.php - load level 2 data

--- index.php
+++ index.php
@@ -8,12 +8,13 @@ require_once 'vendor/autoload.php';
 $importer = new Json();
 
 try {
-    $data = $importer->import(implode(DIRECTORY_SEPARATOR, array(__DIR__, 'part1', 'level1', 'data.json')));
+    $data = $importer->import(implode(DIRECTORY_SEPARATOR, array(__DIR__, 'part1', 'level2', 'data.json')));
 } catch (\Exception $exception) {
     die($exception->getMessage());
 }
 
 $context = new Calculator\Context($data['users'], $data['providers']);
+$context->setContracts($data['contracts']);
 
 $calculator = new Calculator();
 $output = $calculator->calculate($context);
