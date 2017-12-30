e84a685a3fdb2a1a7b0c087f8a09c4dad9732997
haru0 <michal.haracewiat@polcode.net>
2017-12-30 15:35:46 +0100

    Rough calculator

---
+++ src/Calculator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Applicants;
+
+/**
+ * Calculator class.
+ *
+ * @package Applicants
+ * @author Michał Haracewiat <michal.haracewiat@polcode.net>
+ */
+class Calculator
+{
+
+    /**
+     * @param array $users
+     * @param array $providers
+     * @return array
+     */
+    public function calculate(array $users, array $providers): array
+    {
+        $index = 1;
+        $bills = array();
+
+        $providersTransformed = array_column($providers, 'price_per_kwh', 'id');
+
+        foreach ($users as $value) {
+            $bills[] = array(
+                'id' => $index++,
+                'price' => $providersTransformed[$value['provider_id']] * $value['yearly_consumption'],
+                'user_id' => $value['id'],
+            );
+        }
+
+        return array(
+            'bills' => $bills,
+        );
+    }
+
+}
+
