d44e6eca4d2dca4d966e3afe17f723a62f24815d
Michal Haracewiat <m.haracewiat@gmail.com>
2018-01-04 20:54:49 +0100

    Calculating update

--- src/Calculator/Registry.php
+++ src/Calculator/Registry.php
@@ -28,6 +28,11 @@ class Registry
     /**
      * @var array
      */
+    public $usersConsumption;
+
+    /**
+     * @var array
+     */
     public $contractProviders;
 
     /**
@@ -35,19 +40,95 @@ class Registry
      */
     public $greenContracts;
 
+    /**
+     * @var array
+     */
+    public $cancelableProviders;
+
+    /**
+     * @var array
+     */
+    public $canceledProviderContracts = array();
+
+    public $contractDates;
+
 
     /**
      * @param Context $context
      */
     public function register(Context $context)
     {
+        $this->registerContractDates($context->getContracts());
         $this->registerProviderPricings($context->getProviders());
-        $this->registerContractLengths($context->getContracts());
+        $this->registerContractLengths($context->getContracts(), $context->getContractModifications());
         $this->registerContractUsers($context->getContracts());
-        $this->registerContractProviders($context->getContracts());
+        $this->registerUsersConsumption($context->getUsers());
+        $this->registerContractProviders($context->getContracts(), $context->getContractModifications());
         $this->registerGreenContractors($context->getContracts());
+        $this->registerCancelableProviders($context->getProviders());
+        $this->registerContractModifications($context->getContractModifications());
     }
 
+    /**
+     * @param array $contractModifications
+     * @return $this
+     */
+    protected function registerContractModifications(array $contractModifications)
+    {
+        foreach ($contractModifications as $modification) {
+            $contract = $modification['contract_id'];
+            list($start, $end) = array_values($this->contractDates[$contract]);
+
+            if (isset($modification['start_date'])) {
+                $start = \DateTime::createFromFormat('Y-m-d', $modification['start_date']);
+            } else {
+                $start = \DateTime::createFromFormat('Y-m-d', $start);
+            }
+
+            if (isset($modification['end_date'])) {
+                $end = \DateTime::createFromFormat('Y-m-d', $modification['end_date']);
+            } else {
+                $end = \DateTime::createFromFormat('Y-m-d', $end);
+            }
+
+            $length = $this->calculateLength($start, $end);
+
+            if (isset($modification['provider_id'])) {
+                $this->contractLengths[] = $length;
+                $this->contractProviders[] = $modification['provider_id'];
+                $this->contractUsers[] = $this->contractUsers[$contract];
+                $this->canceledProviderContracts[] = $contract;
+            } else {
+                $this->contractLengths[$contract] = $length;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getContracts()
+    {
+        return array_keys($this->contractLengths);
+    }
+
+    /**
+     * @param array $contracts
+     * @return $this
+     */
+    protected function registerContractDates(array $contracts)
+    {
+        $this->contractDates = array_map(
+            function (array $contract) {
+                return array_only($contract, array('start_date', 'end_date'));
+            },
+            array_column($contracts, null, 'id')
+        );
+
+        return $this;
+    }
 
     /**
      * @param array $providers
@@ -61,11 +142,16 @@ class Registry
 
     /**
      * @param array $contracts
+     * @param array $contractModifications
      * @return $this
      */
-    protected function registerContractLengths(array $contracts)
+    protected function registerContractLengths(array $contracts, array $contractModifications)
     {
-        $this->contractLengths = array_column($contracts, 'contract_length', 'id');
+        $this->contractLengths = array_map(
+            array($this, 'mapLength'),
+            array_column($contracts, null, 'id')
+        );
+
         return $this;
     }
 
@@ -80,10 +166,21 @@ class Registry
     }
 
     /**
-     * @param array $contracts
+     * @param array $users
      * @return $this
      */
-    protected function registerContractProviders(array $contracts)
+    protected function registerUsersConsumption(array $users)
+    {
+        $this->usersConsumption = array_column($users, 'yearly_consumption', 'id');
+        return $this;
+    }
+
+    /**
+     * @param array $contracts
+     * @param array $contractModifications
+     * @return $this
+     */
+    protected function registerContractProviders(array $contracts, array $contractModifications)
     {
         $this->contractProviders = array_column($contracts, 'provider_id', 'id');
         return $this;
@@ -95,8 +192,44 @@ class Registry
      */
     protected function registerGreenContractors(array $contracts)
     {
-        $this->greenContracts = array_filter(array_column($contracts, 'green', 'id'), array($this, 'isTrue'));
+        $this->greenContracts = array_filter(
+            array_column($contracts, 'green', 'id'),
+            array($this, 'isTrue')
+        );
+
         return $this;
+    }
+
+    /**
+     * @param array $providers
+     * @return $this
+     */
+    protected function registerCancelableProviders(array $providers)
+    {
+        $this->cancelableProviders = array_filter(
+            array_column($providers, 'cancellation_fee', 'id'),
+            array($this, 'isTrue')
+        );
+
+        return $this;
+    }
+
+
+    /**
+     * @param array $contract
+     * @return string
+     */
+    protected function mapLength(array $contract)
+    {
+        $start = \DateTime::createFromFormat('Y-m-d', $contract['start_date']);
+        $end = \DateTime::createFromFormat('Y-m-d', $contract['end_date']);
+
+        return $this->calculateLength($start, $end);
+    }
+
+    protected function calculateLength(\DateTime $start, \DateTime $end)
+    {
+        return (float)number_format(($end->diff($start)->format('%a') / 365), 2);
     }
 
 
