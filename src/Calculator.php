b2b7661cc18fe0607e689a0b706ee18379f52fcd
Michal Haracewiat <m.haracewiat@gmail.com>
2018-01-07 17:34:24 +0100

    A bit of refactorization

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,9 +4,18 @@ namespace Applicants;
 
 use Applicants\Calculator\Context;
 use Applicants\Calculator\Registry;
+use Applicants\Calculator\RegistryInterface;
+use Applicants\Resolver\Discount as DiscountResolver;
+use Applicants\Resolver\Season as SeasonResolver;
 
 /**
  * Calculator class.
+ *
+ * @method getContractUser(int $contract): int
+ * @method getContractLength(int $contract): float
+ * @method getUserConsumption(int $user): int
+ * @method getContractProvider(int $contract): int
+ * @method isContractCanceled(int $contract): bool
  *
  * @package Applicants
  */
@@ -14,7 +23,40 @@ class Calculator
 {
 
     /**
-     * @var Registry
+     * @var float
+     */
+    const INSURANCE_FEE_FACTOR = .05;
+
+    /**
+     * @var float
+     */
+    const SELECTRA_FEE_FACTOR = .125;
+
+    /**
+     * Value of fee given to canceled contract.
+     *
+     * @var float
+     */
+    const CANCELLATION_CHARGE = 50.0;
+
+
+    /**
+     * Percentage amount of consumption factor in related seasons.
+     *
+     * @var array
+     */
+    protected static $seasonConsumptionHandicaps = array(
+        SeasonResolver::SPRING => .01,
+        SeasonResolver::SUMMER => -.015,
+        SeasonResolver::FALL => .007,
+        SeasonResolver::WINTER => .0,
+    );
+
+
+    /**
+     * Registry holding input data and providing developer-friendly API to perform operations on.
+     *
+     * @var RegistryInterface
      */
     protected $registry;
 
@@ -27,6 +69,21 @@ class Calculator
         $this->registry = new Registry();
     }
 
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call(string $name, array $arguments = array())
+    {
+        if (true == method_exists($this->registry, $name)) {
+            return call_user_func_array(array($this->registry, $name), $arguments);
+        }
+
+        throw new \Exception('Invalid registry method');
+    }
+
 
     /**
      * @param Context $context
@@ -34,11 +91,13 @@ class Calculator
      */
     public function calculate(Context $context): array
     {
-        $this->registry->register($context);
+        $registry = $this->registry;
+        $registry->register($context);
 
         $index = 1;
         $bills = array();
 
+        /** @var int $contract */
         foreach ($this->registry->getContracts() as $contract) {
             $bills[] = array_merge(
                 array('id' => $index++),
@@ -46,82 +105,68 @@ class Calculator
             );
         }
 
+        $registry->clean();
+
         return array(
             'bills' => $bills,
         );
     }
 
+
     /**
-     * @param array $contract
+     * @param int $contract
      * @return array
      */
-    protected function calculateBill($contract): array
+    protected function calculateBill(int $contract): array
     {
+        $price = $this->calculatePrice($contract);
+        $commission = $this->calculateCommission($contract, $price);
+
+        $user = $this->getContractUser($contract);
 
         return array(
-            'price' => ($price = $this->calculatePrice(($user = $this->registry->contractUsers[$contract]), $contract)),
-            'commission' => $this->calculateCommission($contract, $price),
+            'price' => $price,
+            'commission' => $commission,
             'user_id' => $user,
         );
     }
 
     /**
-     * @param $user
-     * @param $contract
+     * @param int $contract
      * @return float
      */
-    protected function calculatePrice($user, $contract): float
+    protected function calculatePrice(int $contract): float
     {
-        $consumption = $this->registry->usersConsumption[$user];
+        $user = $this->getContractUser($contract);
+        $consumption = $this->getUserConsumption($user);
 
-        $length = $this->registry->contractLengths[$contract];
-        $provider = $this->registry->contractProviders[$contract];
+        $length = $this->getContractLength($contract);
+        $provider = $this->getContractProvider($contract);
 
-        $discountFactor = 1;
-
-        switch (true) {
-            case ($length > 3):
-                $discountFactor -= .25;
-                break;
-            case ($length > 1):
-                $discountFactor -= .2;
-                break;
-            case ($length > 0):
-                $discountFactor -= .1;
-                break;
-            default:
-                /* Do nothing. */
-                break;
-        }
-
-        $lengths = $lengths1 = $this->registry->contractSeasons[$contract];
+        $lengths = $this->registry->contractSeasons[$contract];
         $pricing = $this->registry->providerPricings[$provider];
 
+        /* Calculate each season price. Lengths becomes season prices. */
         array_walk(
             $lengths,
-            function (& $days, $season, array $userdata = null) use ($consumption, $pricing, $contract) {
+            function (& $days, $season, array $seasonConsumptionHandicaps = null) use ($consumption, $pricing, $contract) {
                 $seasonConsumption = $consumption * $days / (365 * $this->registry->contractLengths[$contract]);
-                $seasonPricing = ($pricing * (1 + $userdata[$season]));
+                $seasonPricing = ($pricing * (1 + $seasonConsumptionHandicaps[$season]));
 
-                $days = $seasonPricing * $seasonConsumption;
+                $days = ($seasonPricing * $seasonConsumption);
 
                 return;
             },
-            array(
-                'spring' => .01,
-                'summer' => -.015,
-                'fall' => .007,
-                'winter' => .0,
-            )
+            self::$seasonConsumptionHandicaps
         );
 
-        $price = $discountFactor * array_sum($lengths);
+        $price = (DiscountResolver::resolve($length) * array_sum($lengths));
 
         if (true == array_key_exists($contract, $this->registry->greenContracts)) {
             $price -= ($consumption * .05);
         }
 
-        return $price;
+        return round($price, 2);
     }
 
     /**
@@ -131,20 +176,16 @@ class Calculator
      */
     protected function calculateCommission($contract, $price)
     {
-        $length = $this->registry->contractLengths[$contract];
-        $provider = $this->registry->contractProviders[$contract];
+        $length = $this->getContractLength($contract);
 
-        $insuranceFee = (365 * .05 * $length);
+        $insuranceFee = (365 * $length * self::INSURANCE_FEE_FACTOR);
         $providerFee = ($price - $insuranceFee);
 
-        if (
-            (true == @$this->registry->cancelableProviders[$provider]) &&
-            (true == in_array($contract, $this->registry->canceledProviderContracts))
-        ) {
-            $providerFee += 50;
+        if (true == $this->isContractCanceled($contract)) {
+            $providerFee += self::CANCELLATION_CHARGE;
         }
 
-        $selectraFee = ($providerFee * .125);
+        $selectraFee = ($providerFee * self::SELECTRA_FEE_FACTOR);
 
         return array(
             'insurance_fee' => round($insuranceFee, 2),
